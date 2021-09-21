b8e0d77795044e05f3efbc5f0842f695e76213f6
kayleereid <rei18018.byui.edu>
2021-09-21 14:27:02 -0600

    home page

---
+++ js/dates.js
@@ -0,0 +1,6 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+var dateupdated= document.getElementById("dateupdated");
+dateupdated.textContent = document.lastModified;
+
