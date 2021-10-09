d992c9234f60d83a098dcf0f2ce684d2e66e1922
kayleereid <rei18018.byui.edu>
2021-10-09 15:09:37 -0600

    change

--- lesson4/js/getdate.js
+++ lesson4/js/getdate.js
@@ -1,14 +1,12 @@
-document.querySelector('.bar-icon').addEventListener('click', () => {
-    document.querySelector('.navigation').classList.toggle('responsive');
-}, false);
-
-window.onresize = () => {
-    if (window.innerWidth > 675) document.querySelector('.navigation').classList.remove('responsive')
-};
-
 const date = new Date(Date.now());
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
 
-document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
+document.querySelector("#cDate").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#copyrightYear").textContent = date.getFullYear();
