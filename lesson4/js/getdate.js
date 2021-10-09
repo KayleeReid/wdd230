4d0647d6ec61f9a1aae5c3c3f55492d2fa3d464e
kayleereid <rei18018.byui.edu>
2021-10-09 13:13:36 -0600

    change

---
+++ lesson4/js/getdate.js
@@ -0,0 +1,8 @@
+document.getElementById("copyrightYear").textContent = new Date().getFullYear();
+
+const options = {
+    weekday: 'long',
+    day: 'short',
+    month: 'medium',
+    year: 'numeric'
+}
