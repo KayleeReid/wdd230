22a0fbb8cbd63919e6e5081f5b493ff4bebc0b01
kayleereid <rei18018.byui.edu>
2021-10-09 15:41:22 -0600

    oush

--- lesson4/js/getdate.js
+++ lesson4/js/getdate.js
@@ -1,3 +1,15 @@
 const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
 
+const date = new Date(Date.now());
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.querySelector("#cDate").textContent = date.toLocaleDateString(
+  "en-UK",
+  options
+);
