6f9d0737fa1ef07ff0b50e0cc8f60e535b70291c
kayleereid <rei18018.byui.edu>
2021-10-22 14:30:49 -0600

    add

--- lesson5/js/pancakes.js
+++ lesson5/js/pancakes.js
@@ -3,6 +3,6 @@
 const day = new Date();
 
 if (day.getDay() == 5) {
-    document.getElementsByClassName("message").style.display = 'block';
+    document.querySelector(".message").style.display = 'block';
 }
 
