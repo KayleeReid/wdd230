ee50fec2fa4f47775375f8ab174d041056fe764f
kayleereid <rei18018.byui.edu>
2021-11-12 20:04:18 -0700

    add

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,32 +1,41 @@
+const requestURL =
+  "https://byui-cit230.github.io/weather/data/towndata.json";
 
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
+
   .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const prophets = jsonObject["towns"];
-    for (let i = 0; i < prophets.length; i++) {
+    const towns = jsonObject["towns"];
+    
+    const townName = towns.filter((town) => town.name ==='Preston' || town.name ==='Soda Springs' || town.name ==='Fish Haven');
+    townName.forEach((town) => { 
+      
+      //this creates the elements in HTML
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let h3 = document.createElement("h3");
       let year = document.createElement("p");
       let pop = document.createElement("p");
       let rain = document.createElement("p");
+      let img = document.createElement("img");
 
-      h2.textContent = towns[i].name;
-      h3.textContent = towns[i].motto;
-      year.textContent = towns[i].yearfounded;
-      pop.textContent = towns[i].currentPopulation;
-      rain.textContent = towns[i].averageRainfall;
+      h2.innerHTML = `${town.name}`;
+      h3.innerHTML = `${town.motto}`;
+      year.innerHTML = `Year Founded: ${town.yearfounded}`;
+      pop.innerHTML= `Population: ${town.currentPopulation}`;
+     rain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`
+      img.setAttribute ('src', town.photo);
 
       card.appendChild(h2);
       card.appendChild(h3);
       card.appendChild(year);
       card.appendChild(pop);
       card.appendChild(rain);
+      card.appendChild(img);
 
-      document.querySelector("div.cards").appendChild(card);
-    }
+      document.querySelector("div.townInfo").appendChild(card);
+    });
   });
