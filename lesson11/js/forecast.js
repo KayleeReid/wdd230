369ee80766418b33560e6baef8e3e3cf043bd936
kayleereid <rei18018.byui.edu>
2021-12-06 16:12:54 -0700

    add

--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,41 +1,46 @@
-let cityURL = ""; 
-let cityPage = document.querySelector("h2").textContent;
-if (cityPage === "The town of Preston Idaho"){
-  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+if (cityPage === "The Town of Preston Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
   // Preston city = "5604473"
-}else if (cityPage === "The Town of Soda Springs Idaho"){ 
-  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+} else if (cityPage === "The Town of Soda Springs Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
   // SodaSprings city = "5607916"
-}else if (cityPage === "The Town of Fish Haven Idaho"){
-  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+} else if (cityPage === "The Town of Fish Haven Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
   // FishHaven city = "5585010"
-};
+}
 
-const api = cityURL;
-fetch(api)
-.then((response) => response.json())
-.then((jsObject) => {
+fetch(cityURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const time = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00"));
 
-    const time = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-    
+    const weekdays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    
     let day = 0;
-    time.forEach(forecast => {
-        
-        let thedate = new Date(forecast.dt_txt);
-        let imagesrc = 'https://openweathermap.org/img/wn/' + forecast.weather[0].icon + '.png'; 
-        const description = forecast.weather[0].description;
+    time.forEach((forecast) => {
+      let thedate = new Date(forecast.dt_txt);
+      let imagesrc =
+        "https://openweathermap.org/img/wn/" +
+        forecast.weather[0].icon +
+        ".png";
+      const description = forecast.weather[0].description;
 
-        document.querySelector(`#weekday${day+1}`).textContent = weekdays[thedate.getDay()];
-        document.querySelector(`#forecast${day+1}`).textContent = forecast.main.temp.toFixed(0) + "°F";
-        document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-        document.getElementById(`icon${day+1}`).setAttribute('alt', description);
-        day++;
-        
-   
-});
+      document.querySelector(`#weekday${day + 1}`).textContent =
+        weekdays[thedate.getDay()];
+      document.querySelector(`#forecast${day + 1}`).textContent =
+        forecast.main.temp.toFixed(0) + "°F";
+      document.getElementById(`icon${day + 1}`).setAttribute("src", imagesrc);
+      document
+        .getElementById(`icon${day + 1}`)
+        .setAttribute("alt", description);
+      day++;
     });
-
-
+  });
--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -1,23 +1,22 @@
-let cityURL = ""; 
+let cityURL = "";
 let cityPage = document.querySelector("h2").textContent;
-if (cityPage === "The town of Preston Idaho"){
-  cityURL = `http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+if (cityPage === "The Town of Preston Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // Preston city = "5604473"
-}else if (cityPage === "The Town of Soda Springs Idaho"){ 
-  cityURL = `http://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+} else if (cityPage === "The Town of Soda Springs Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // SodaSprings city = "5607916"
-}else if (cityPage === "The Town of Fish Haven Idaho"){
-  cityURL = `http://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+} else if (cityPage === "The Town of Fish Haven Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // FishHaven city = "5585010"
 };
 
- 
-const apiURL = cityURL;
-fetch(apiURL)
+
+
+fetch(cityURL)
   .then((response) => response.json())
   .then((jsObject) => {
-   
-    
+    console.log(jsObject);
 
     document.querySelector('#C').textContent = jsObject.weather[0].main;
     document.querySelector('#temp').textContent = `${jsObject.main.temp.toFixed(0)} ℉`;
@@ -27,7 +26,10 @@ fetch(apiURL)
     const w = parseFloat(jsObject.wind.speed.toFixed(0));
     const wcfactor = windChill(t, w);
 
-  function windChill(t, w){
+    document.querySelector('#wChill').innerHTML = wcfactor;
+  });
+
+  function windChill(t, w) {
     let wcfactor = 0
     let a = 35.74;
     let b = 0.6215;
@@ -35,16 +37,13 @@ fetch(apiURL)
     let d = 0.16;
     let e = 0.4275;
 
-    if (t<=50 && w>3){
-        wcfactor= a + (t * b)
-        - (c * Math.pow(w,d))
-        + (e * t * Math.pow(w,d ))
-        wcfactor= `${wcfactor.toFixed(0)}℉`
-    }else{wcfactor= "N/A"}
-    
+    if (t <= 50 && w > 3) {
+      wcfactor = a + (t * b)
+        - (c * Math.pow(w, d))
+        + (e * t * Math.pow(w, d))
+      wcfactor = `${wcfactor.toFixed(0)}℉`
+    } else { wcfactor = "N/A" }
+
     return wcfactor;
   }
-  document.querySelector('#wChill').innerHTML = wcfactor;
-  });
 
- 
