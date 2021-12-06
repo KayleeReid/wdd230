10ea69641b0412edc0c3e05212f6f87b96dfb538
kayleereid <rei18018.byui.edu>
2021-12-06 11:17:47 -0700

    add

--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,23 +1,23 @@
 let cityURL = ""; 
 let cityPage = document.querySelector("h2").textContent;
 if (cityPage === "The town of Preston Idaho"){
-  cityURL = `http://pro.openweathermap.org/data/2.5/forecast/hourly?id=5604473&units=imperial&appid=e3fb9695ca3ae44886d055c51f30dcb2`
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // Preston city = "5604473"
 }else if (cityPage === "The Town of Soda Springs Idaho"){ 
-  cityURL = `http://pro.openweathermap.org/data/2.5/forecast/hourly?id=5607916&units=imperial&appid=e3fb9695ca3ae44886d055c51f30dcb2`
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // SodaSprings city = "5607916"
 }else if (cityPage === "The Town of Fish Haven Idaho"){
-  cityURL = `http://pro.openweathermap.org/data/2.5/forecast/hourly?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`
   // FishHaven city = "5585010"
 };
 
-const api = fURL;
+const api = cityURL;
 fetch(api)
 .then((response) => response.json())
 .then((jsObject) => {
 
     const time = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-    console.log(jsObject);
+    
 
     const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     
@@ -37,3 +37,5 @@ fetch(api)
    
 });
     });
+
+
