6fc5e1ed752c7ea2b3f0ca4b341b7b3861b9ef12
kayleereid <rei18018.byui.edu>
2021-12-09 09:10:35 -0700

    add

---
+++ final/js/forecast.js
@@ -0,0 +1,46 @@
+if (cityPage === "The Town of Preston Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
+  // Preston city = "5604473"
+} else if (cityPage === "The Town of Soda Springs Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
+  // SodaSprings city = "5607916"
+} else if (cityPage === "The Town of Fish Haven Idaho") {
+  cityURL = `https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=789014a950c83560c0a72965a5dfd442`;
+  // FishHaven city = "5585010"
+}
+
+fetch(cityURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const time = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00"));
+
+    const weekdays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = 0;
+    time.forEach((forecast) => {
+      let thedate = new Date(forecast.dt_txt);
+      let imagesrc =
+        "https://openweathermap.org/img/wn/" +
+        forecast.weather[0].icon +
+        ".png";
+      const description = forecast.weather[0].description;
+
+      document.querySelector(`#weekday${day + 1}`).textContent =
+        weekdays[thedate.getDay()];
+      document.querySelector(`#forecast${day + 1}`).textContent =
+        forecast.main.temp.toFixed(0) + "°F";
+      document.getElementById(`icon${day + 1}`).setAttribute("src", imagesrc);
+      document
+        .getElementById(`icon${day + 1}`)
+        .setAttribute("alt", description);
+      day++;
+    });
+  });
